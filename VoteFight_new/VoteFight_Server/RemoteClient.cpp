90db7b04da7d198200cca77c6bf3b6cb5b6f70b9
Eom Mi Yeong <aldjd020420@naver.com>
2024-05-04 05:50:58 +0900

    Delete old server project

--- VoteFight_new/VoteFight_Server/RemoteClient.cpp
+++
@@ -1,5 +0,0 @@
-#include "pch.h"
-#include "../VoteFight_IOCP/ImaysNet/ImaysNet.h"
-#include "RemoteClient.h"
-
-unordered_map<RemoteClient*, shared_ptr<RemoteClient>> RemoteClient::m_remoteClients;
